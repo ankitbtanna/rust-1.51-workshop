66c430a63ea392b3077e593a642a494182055d2b
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-10-22 15:47:26 +1000

    solve the problem using slices

---
+++ part6/src/main-solution2.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
+
+    let sum_of_nums = sum(numbers.as_slice());
+    let product_of_nums = product(numbers.as_slice()); // 👉 TODO call product() and put answer here
+    let average_of_nums = average(numbers.as_slice()); // 👉 TODO call average() and put answer here
+    
+    // Option 2: Accept a slice - that is, &[i64] - instead of a Vec
+
+    println!("Sum of these numbers: {}", sum_of_nums);
+    println!("Product of these numbers: {}", product_of_nums);
+    println!("Average of these numbers: {}", average_of_nums);
+}
+
+fn sum(numbers: &[i64]) -> i64 {
+    let mut total = 0;
+
+    for num in numbers.iter() {
+        total += num;
+    }
+
+    total
+}
+
+fn product(numbers: &[i64]) -> i64 {
+    let mut total = 1;
+
+    for num in numbers.iter() {
+        total *= num;
+    }
+
+    total
+}
+
+fn average(numbers: &[i64]) -> i64 {
+    let length = numbers.len() as i64;
+
+    sum(numbers) / length
+}
